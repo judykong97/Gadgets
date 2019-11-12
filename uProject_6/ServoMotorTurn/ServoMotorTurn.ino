2a3a01f9840958823e3a70cd9212c9ef5223abce
Judy Kong <junhank@andrew.cmu.edu>
2019-11-11 20:58:24 -0500

    Done with uProject 6

--- uProject_6/ServoMotorTurn/ServoMotorTurn.ino
+++ uProject_6/ServoMotorTurn/ServoMotorTurn.ino
@@ -23,46 +23,27 @@
 */
 /*========================================================*/
 
+#include <Servo.h>
+
 /*------- Hardware configuration -------*/
-//const int  PinA = 2;
-//const int  PinB = 3;
-//const int  PinC = 4;
-//const int  PinD = 5;
-// const int  switchPinLR = 6;        // pin with switch attached
-// const int  switchPinRL = 7;        // pin with switch attached
+Servo servo_test;
+const int  motorPin = 9; 
 const int  potPin = A4; 
 const byte debugPin = 13;        // pin that we put debug output on (set to 255 to disable)
                                  // (most Arduino's have a built in LED on pin 13...)
 
-/*------- Knob reader -------*/
-void dynamicDelay(int angle1024) {
-  delay((250 + angle1024));
-}
-
 /*-------------------------------------------*/
 /* Initializization code (run once via call from Arduino framework) */
 void setup() {
-
   Serial.begin(9600);
   Serial.println("Hello world!");
-  
-  // establish direction of pins we are using to drive LEDs 
-//  pinMode(PinA, OUTPUT); 
-//  pinMode(PinB, OUTPUT); 
-//  pinMode(PinC, OUTPUT); 
-//  pinMode(PinD, OUTPUT); 
-  
+  servo_test.attach(motorPin);
   pinMode(debugPin, OUTPUT);
 }
 
 /* Main routine (called repeated by from the Arduino framework) */
 void loop() {
-
   Serial.println(analogRead(potPin));
-//  digitalWrite(PinA,HIGH); dynamicDelay(analogRead(potPin)); digitalWrite(PinA,LOW);
-//  digitalWrite(PinB,HIGH); dynamicDelay(analogRead(potPin)); digitalWrite(PinB,LOW);
-//  digitalWrite(PinC,HIGH); dynamicDelay(analogRead(potPin)); digitalWrite(PinC,LOW);
-//  digitalWrite(PinD,HIGH); dynamicDelay(analogRead(potPin)); digitalWrite(PinD,LOW);
-  
-  delay(400);
+  servo_test.write(map(analogRead(potPin), 0, 1024, 180, 0));
+  // delay(400);
 }
